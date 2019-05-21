8828fe181fe91702cdda8543e6bacdcdfbc09de3
Adam Wickenden <adam.wickenden@accenture.com>
2019-05-21 15:38:00 +0100

    added termination messages

--- 02-notifon/notif/handler.py
+++ 02-notifon/notif/handler.py
@@ -1,8 +1,9 @@
 import os
 import requests
 
-def post_to_slack(event, context):
-    slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
+slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
+
+def post_to_slack_launch(event, context):    
 
     #format.(**event) => glob(**) operator takes the event as a dictionary and allows it to be passed into the string
     slack_message = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
@@ -12,3 +13,12 @@ def post_to_slack(event, context):
 
     return
 
+def post_to_slack_terminate(event, context):
+
+    #format.(**event) => glob(**) operator takes the event as a dictionary and allows it to be passed into the string
+    slack_message = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
+
+    data = {"text":slack_message}
+    requests.post(slack_webhook_url, json=data)
+
+    return
